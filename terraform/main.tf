e376b65f7c6faf36fd24de711e2a357ae70db496
Learner <learner@acelro.com>
2026-07-30 21:49:46 +0000

    Step 1: EKS cluster + Ray cluster config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,13 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
 }
 
 resource "aws_eks_cluster" "ray_cluster" {
   name     = "ray-eks-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = var.private_subnet_ids
-  }
+  vpc_config { subnet_ids = var.private_subnet_ids }
 }
 
 resource "aws_eks_node_group" "ray_node_group" {
@@ -22,10 +16,5 @@ resource "aws_eks_node_group" "ray_node_group" {
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.private_subnet_ids
   instance_types  = ["m5.xlarge"]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 5
-    min_size     = 1
-  }
+  scaling_config { desired_size = 2, max_size = 5, min_size = 1 }
 }
